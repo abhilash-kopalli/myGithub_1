b40f25f9a45629203e8a5d0462d3ef59988eb60b
abhilash-kopalli <kopalliabhilash@gmail.com>
2016-01-04 19:31:36 +0530

    adding test.cpp

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+ int a;
+ std::cout << "Hello BALA" << std::endl;
+ std::cin >> a;
+ std::cout << "You enetered number " << a << " . FUCK OFF!! " << std::endl;
+
+return 0;	 
+}
